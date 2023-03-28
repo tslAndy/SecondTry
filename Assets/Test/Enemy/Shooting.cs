72d5dc86a8ba7fcf0202f4e2a685ec9c6c0e6aaf
Andy <andy.tsl@pm.me>
2023-03-28 14:56:44 +0300

    Shoot start

---
+++ Assets/Test/Enemy/Shooting.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    [SerializeField] private float shootRate;
+
+    float lastShootedTime;
+}
