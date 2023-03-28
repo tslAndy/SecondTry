3800416fe1344bb963718b052e8d6aeada82cea8
Andy <andy.tsl@pm.me>
2023-03-28 10:00:08 +0300

    Destroyable obstacle

---
+++ Assets/Test/Level/DestroyableObstacle.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyableObstacle : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            Destroy(gameObject);
+    }
+}
